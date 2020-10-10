50e9c2bc014535221a566e0c7a4deb11a0028f6d
fernando-meira <thomwork@outlook.com>
2020-10-10 15:54:10 -0300

    feat: 🎸 Added function to setTheme in localStorage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React, { useState, useCallback } from 'react';
+import React, { useCallback } from 'react';
 import { ThemeProvider } from 'styled-components';
 import dark from './styles/themes/dark';
 import light from './styles/themes/light';
+import usePersistedState from './functions/usePersistedState';
 
 import GlobalStyle from './styles/global';
 import Main from './pages/Main';
 
 function App() {
-  const [theme, setTheme] = useState(dark);
+  const [theme, setTheme] = usePersistedState('theme', dark);
 
   const toggleTheme = useCallback(() => {
     setTheme(theme.title === 'dark' ? light : dark);
-  }, [theme.title]);
+  }, [theme.title, setTheme]);
 
   return (
     <ThemeProvider theme={theme}>
